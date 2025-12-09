f79b86840719383262b1df08ccd966aa5bded328
Olga Noskova <olga.tolba@gmail.com>
2025-12-09 13:13:38 +1000

    Extend CourseSerializer with lessons list and lessons_count

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'description', 'preview', 'lessons_count']
-
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'preview', 'lessons_count', 'lessons']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
